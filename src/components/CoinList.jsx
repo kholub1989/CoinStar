43d09441c95c2608a35b12a07a27841c1d1cac1f
Krisztian Holub <kholub1989@gmail.com>
2020-09-30 21:52:48 -0400

    Join Watchlist

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -12,7 +12,7 @@ const CoinList = () => {
       const response = await coinGecko.get("/coins/markets", {
         params: {
           vs_currency: "usd",
-          ids: "bitcoin",
+          ids: watchList.join(","),
         },
       });
       console.log(response.data);
