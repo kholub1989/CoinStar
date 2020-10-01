d818d21b904138bbafa90fd7f9310901d7cd2532
Krisztian Holub <kholub1989@gmail.com>
2020-09-30 20:58:07 -0400

    CoinGecko API

---
+++ src/components/CoinList.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import coinGecko from "../apis/coinGecko";
+
+const CoinList = () => {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await coinGecko.get("/coins/markets", {
+        params: {
+          vs_currency: "usd",
+          ids: "bitcoin, ethereum",
+        },
+      });
+      console.log(response.data);
+    };
+    fetchData();
+  }, []);
+
+  return <div></div>;
+};
+
+export default CoinList;
--- src/pages/CoinSummaryPage.jsx
+++ src/pages/CoinSummaryPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import AddCoin from "../components/AddCoin";
+import CoinList from "../components/CoinList";
 
 const CoinSummaryPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <AddCoin />
+      <CoinList />
+    </div>
+  );
 };
 
 export default CoinSummaryPage;
